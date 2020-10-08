da43fb37eda706d336cc946e0ae629fd49fada88
Isabel Tiburski <itiburski@gmail.com>
2020-10-08 08:32:32 -0300

    Fix TaskResource @Path

--- atask-rest/src/main/java/br/com/jitec/atask/resource/task/TaskResource.java
+++ atask-rest/src/main/java/br/com/jitec/atask/resource/task/TaskResource.java
@@ -16,7 +16,7 @@ import javax.ws.rs.core.Response.Status;
 import br.com.jitec.atask.business.task.Task;
 import br.com.jitec.atask.business.task.TaskBeanLocal;
 
-@Path("/task")
+@Path("/tasks")
 @Consumes({ MediaType.APPLICATION_JSON })
 @Produces({ MediaType.APPLICATION_JSON })
 public class TaskResource {
